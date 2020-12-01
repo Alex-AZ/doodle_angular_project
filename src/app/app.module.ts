85a0261f91a40c79f0e529ee2c19d985afac33d5
Alexis <alex1505@sfr.fr>
2020-12-01 14:33:50 +0100

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SurveyService } from './services/survey.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
-  providers: [],
+  providers: [
+    SurveyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
